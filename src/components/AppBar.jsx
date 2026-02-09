80b403895eaab1fa551d899f86d01e5af52f380d
William-nyarash <owetywilliamnyaranga@gmail.com>
2026-02-09 11:26:18 +0300

    final part 2

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,26 +1,44 @@
-import { View, StyleSheet, Pressable } from 'react-native';
-import Constants from 'expo-constants';
+import { View,ScrollView, StyleSheet, Pressable } from 'react-native';
+import { Link } from 'react-router-native';
+import Text from './Text'; 
+import theme from '../theme';
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop:19,
-    paddingLeft: 20,
+    paddingTop: 19,
+    paddingHorizontal: 20,
     backgroundColor: "#24292e",
-    paddingBottom:30
+    paddingBottom: 30,
+    display: "flex",
+    width:'100%',
+    justifyContent: "space-between",
+    flexDirection: "row",
   },
   text: {
-    color: "white",
+    color: theme.colors.textSecondary,
     fontWeight: "bold",
-    fontSize: 30
-},
+    fontSize: 30,
+    paddingHorizontal: 10
+  },
+  scroll: {
+    padding: 12,
+  
+  }
 });
 
 const AppBar = () => {
-  return <View style={styles.container}>
-    <Pressable style={styles.text} >
-        Repository
-    </Pressable>
-  </View>;
+  return (
+    <View style={styles.container}>
+    <ScrollView horizontal>
+      <Pressable>
+        <Text style={styles.text}>Repository</Text>
+      </Pressable>
+      <Link to='/signin'>
+        <Text style={styles.text}>Sign In</Text>
+      </Link>
+     </ScrollView>
+  </View>
+  );
 };
 
 export default AppBar;
