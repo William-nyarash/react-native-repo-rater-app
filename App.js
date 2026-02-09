763e02157330de1e8e0ff594afafdfea385ef4e6
William-nyarash <owetywilliamnyaranga@gmail.com>
2026-02-09 11:25:29 +0300

    add router

--- App.js
+++ App.js
@@ -1,7 +1,17 @@
+import { StatusBar } from 'react-native-web';
 import Main from './src/components/Main';
+import { NativeRouter } from 'react-router-native';
 
 const App =() => {
-  return(<Main />)
+  return(
+  <>
+    <NativeRouter>
+       <Main />
+    </NativeRouter>
+    <StatusBar style="auto" />
+  </>
+)
+
 }
 
 export default App ;
