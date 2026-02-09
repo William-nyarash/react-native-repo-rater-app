c490169598226b9b57937361eb0b0778867e1953
William-nyarash <owetywilliamnyaranga@gmail.com>
2026-02-09 11:26:18 +0300

    final part 2

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,23 +1,30 @@
-import Constants from 'expo-constants';
-import { Text, StyleSheet, View } from 'react-native';
+// import Constants from 'expo-constants';
+import { StyleSheet, Text, View } from 'react-native';
+import { Route, Routes, Navigate, Link } from 'react-router-native';
+
 import RepositoryList from './RepositoryList';
 import AppBar from './AppBar';
+import theme from '../theme';
+import SignIn from './SignIn';
 
 const styles = StyleSheet.create({
   container: {
-    // marginTop: Constants.statusBarHeight,
     flexGrow: 1,
     flexShrink: 1,
-    backgroundColor: "#e1e4e8"
+    backgroundColor: theme.colors. mainBackground,
   },
 });
 
 const Main = () => {
   return (
-    <View style={styles.container}>
-      <AppBar />
-      <RepositoryList />
-    </View>
+      <View style={styles.container}>  
+        <AppBar />
+        <Routes>
+          <Route path='/' element={<RepositoryList />} />
+          <Route path='/signin' element={<SignIn />} />
+          <Route path='*' element={<Navigate to="/" replace />} />
+        </Routes>
+      </View>
   );
 };
 
